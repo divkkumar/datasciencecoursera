3ce72d2e6bf62a37437a474f429635d25334b4b9
divkkumar <50437436+divkkumar@users.noreply.github.com>
2019-06-12 17:32:16 +0530

    Add files via upload

---
+++ meanpollutant.R
@@ -0,0 +1,30 @@
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
+}
